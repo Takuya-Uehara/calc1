ed02583c7f3d6b75699d10290a4780f732b76145
nf_uehara <nn.uehara320@gmail.com>
2020-09-19 11:54:30 +0900

    複数演算子に適応

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Hello {
 
   public static void main(String[] args) {
-
 
     //起動時説明を出力する
     System.out.println("数式を入力してください。(例)1+1");
@@ -17,66 +16,52 @@ public class Hello {
       String formula = scanner.nextLine();
       System.out.println(formula);
 
-      //演算子を探す&終了キーを探す
-
-      String[] numberList =formula.split("[¥+¥-¥*/]");
-      System.out.println(numberList[1]);
-
-      int order = 0;
-      int calc = 0;
-      if (formula.contains("+")) {
-        order = formula.indexOf("+");
-        calc = 1;
-      } else if (formula.contains("-")) {
-        order = formula.indexOf("-");
-        calc = 2;
-      } else if (formula.contains("*")) {
-        order = formula.indexOf("*");
-        calc = 3;
-      } else if (formula.contains("/")) {
-        order = formula.indexOf("/");
-        calc = 4;
-      } else if (formula.contains("end")) {
+      //endを検出しプログラムを終わらせる
+      if (formula.contains("end")) {
         System.exit(0);
-      } else {
-        System.out.println("演算子が検出されません。数式を確認してください");
-        caluculation();
       }
 
-      //演算子の左右で入力を文字列として抽出する
-      String input1 = formula.substring(0, order);
-      String input2 = formula.substring(order + 1);
-
-      //抽出した文字列を計算可能な型に変換する
-      double newInput1 = Double.parseDouble(input1);
-      double newInput2 = Double.parseDouble(input2);
-
-      //計算する
-      double result = 0;
-      if (calc == 1) {
-        result = newInput1 + newInput2;
-      } else if (calc == 2) {
-        result = newInput1 - newInput2;
-      } else if (calc == 3) {
-        result = newInput1 * newInput2;
-      } else if (calc == 4 && newInput2 != 0) {
-        result = newInput1 / newInput2;
-      } else if (calc == 4 && newInput2 == 0) {
-        System.out.println("0で除算をすることはできません。やり直してください");
-        caluculation();
+      //数字だけを抽出してdouble型の配列として出力
+      String[] numberList =formula.split("[¥+¥-¥*/]");
+      double[] doubleNumber = new double[numberList.length];
+      for (int i = 0; i < numberList.length; i++) {
+        doubleNumber[i] = Double.parseDouble(numberList[i]);
       }
 
-      //計算結果が整数の場合、int型に直して結果を表示する
+      //演算子だけを抽出
+      String[] operatorList = formula.split("[1234567890¥.]+");
+      System.out.println(Arrays.deepToString(operatorList));
+
+      //計算をさせる
+      int n = 1;
+      double total = doubleNumber[0];
+
+      for (n=1;n<operatorList.length;n++) {
+        if (operatorList[n].contains("+")) {
+          total = total + doubleNumber[n];
+        } else if (operatorList[n].contains("-")) {
+          total = total - doubleNumber[n];
+        } else if (operatorList[n].contains("*")) {
+          total = total * doubleNumber[n];
+        } else if (operatorList[n].contains("/") && doubleNumber[n]!=0) {
+          total = total / doubleNumber[n];
+        } else if (operatorList[n].contains("/") && doubleNumber[n]==0) {
+          System.out.println("0で除算をすることはできません。やり直してください");
+          caluculation();
+        }
+      }
       int integer = 0;
-      if (result % 1 == 0) {
-        integer = (int) result;
+      if (total % 1 == 0) {
+        integer = (int) total;
         System.out.println(integer);
       } else {
-        System.out.println(result);
+        System.out.println(total);
       }
 
       //もう一度計算させる
       caluculation();
     }
 
-}
+ }
+
+
