cadfcacd3d65e080b8850df7760bdf89989a4afa
nf_uehara <nn.uehara320@gmail.com>
2020-09-21 16:39:38 +0900

    ”計算の優先順位をつけて計算をする”を実装

--- src/Hello.java
+++ src/Hello.java
@@ -7,10 +7,12 @@ public class Hello {
     //起動時説明を出力する
     System.out.println("数式を入力してください。(例)1+1");
     System.out.println("プログラムを終了したい場合、endと書いてください。");
-    caluculation();
+    calculation();
   }
+
+
     //計算式を入力
-    public static void caluculation (){
+    public static void calculation (){
       Scanner scanner = new Scanner(System.in);
       System.out.print("計算式：");
       String formula = scanner.nextLine();
@@ -21,47 +23,79 @@ public class Hello {
         System.exit(0);
       }
 
-      //数字だけを抽出してdouble型の配列として出力
-      String[] numberList =formula.split("[¥+¥-¥*/]");
-      double[] doubleNumber = new double[numberList.length];
-      for (int i = 0; i < numberList.length; i++) {
-        doubleNumber[i] = Double.parseDouble(numberList[i]);
-      }
+      //+-で分割する
+      String[] splitFormula =formula.split("[¥+-]");
+      //System.out.println(Arrays.deepToString(splitFormula));
 
-      //演算子だけを抽出
-      String[] operatorList = formula.split("[1234567890¥.]+");
-      System.out.println(Arrays.deepToString(operatorList));
+      //+-の演算子を記録しておく
+      String[] additionAndSubtraction = formula.split("[ 1234567890/¥.*]+");
+      //System.out.println(Arrays.deepToString(additionAndSubtraction));
 
-      //計算をさせる
-      int n = 1;
-      double total = doubleNumber[0];
+      //分割された中身で掛け算割り算を行う
+      Double[] splitTotal = new Double [splitFormula.length];
+      for(int a= 0; a<splitFormula.length ;a++) {
 
-      for (n=1;n<operatorList.length;n++) {
-        if (operatorList[n].contains("+")) {
-          total = total + doubleNumber[n];
-        } else if (operatorList[n].contains("-")) {
-          total = total - doubleNumber[n];
-        } else if (operatorList[n].contains("*")) {
-          total = total * doubleNumber[n];
-        } else if (operatorList[n].contains("/") && doubleNumber[n]!=0) {
-          total = total / doubleNumber[n];
-        } else if (operatorList[n].contains("/") && doubleNumber[n]==0) {
-          System.out.println("0で除算をすることはできません。やり直してください");
-          caluculation();
+        //分割されたから数字のみを抽出
+        String[] numberList=splitFormula[a].split("[/¥*]");
+
+
+        //抽出した数字をdoubleになおす
+        double[] doubleNumber = new double[numberList.length];
+        for (int i = 0; i < numberList.length; i++) {
+          doubleNumber[i] = Double.parseDouble(numberList[i]);
+        }
+
+        //数字が一つの場合、処理を中断してsplitTotalに代入
+        if (doubleNumber.length==1){
+          splitTotal[a]=doubleNumber[0];
+          continue;
+        }
+
+        //演算子だけを抽出
+        String[] operatorList = splitFormula[a].split("[1234567890¥.]+");
+
+
+        //計算をさせ、結果をsplitTotalに保存
+        double semiTotal = doubleNumber[0];
+        for (int n=1;n<operatorList.length;n++) {
+          if (operatorList[n].contains("*")) {
+            semiTotal = semiTotal * doubleNumber[n];
+          } else if (operatorList[n].contains("/") && doubleNumber[n]!=0) {
+            semiTotal= semiTotal / doubleNumber[n];
+          } else if (operatorList[n].contains("/") && doubleNumber[n]==0) {
+            System.out.println("0で除算をすることはできません。やり直してください");
+            calculation();
+          }
+          splitTotal[a]=semiTotal;
         }
       }
+
+
+      //最終計算を行う
+      double total = splitTotal[0];
+      for (int p=1 ;p< additionAndSubtraction.length;p++ ){
+        if (additionAndSubtraction[p].contains("+")){
+          total+=splitTotal[p];
+        }else if(additionAndSubtraction[p].contains("-")){
+          total-=splitTotal[p];
+        }
+      }
+
+
+      //整数を選別し型を変換する
       int integer = 0;
       if (total % 1 == 0) {
         integer = (int) total;
-        System.out.println(integer);
+        System.out.println("計算結果:"+integer);
       } else {
-        System.out.println(total);
+        System.out.println("計算結果:"+total);
       }
 
-      //もう一度計算させる
-      caluculation();
+      calculation();
     }
 
- }
+
+
+  }
 
 
